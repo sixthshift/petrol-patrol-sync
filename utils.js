dd8c757022e08491e0ca1d736b5304faebeab8e5
Jason Huang <sixth.shifts@gmail.com>
2018-06-27 18:36:25 +1000

    Refactored utils.js

--- utils.js
+++ utils.js
@@ -3,41 +3,30 @@ const crypto = require('crypto');
 
 module.exports = {
     currentTimestamp: () => {
-        var now = new Date();
-        var year = now.getFullYear();
-        var month = now.getMonth() + 1;
-        var day = now.getDate();
-        var hour = now.getHours();
-        var minute = now.getMinutes();
-        var second = now.getSeconds();
-        if (month.toString().length == 1) {
-            var month = '0' + month;
-        }
-        if (day.toString().length == 1) {
-            var day = '0' + day;
-        }
-        if (hour.toString().length == 1) {
-            var hour = '0' + hour;
-        }
-        if (minute.toString().length == 1) {
-            var minute = '0' + minute;
-        }
-        if (second.toString().length == 1) {
-            var second = '0' + second;
-        }
+        const now = new Date();
+        let year = now.getFullYear();
+        let month = now.getMonth() + 1;
+        let day = now.getDate();
+        let hour = now.getHours();
+        let minute = now.getMinutes();
+        let second = now.getSeconds();
 
-        var dateTime = day + '/' + month + '/' + year + ' ' + hour + ':' + minute + ':' + second;
-        return dateTime;
+        month = _.padStart(month, 2, '0');
+        day = _.padStart(day, 2, '0');
+        hour = _.padStart(hour, 2, '0');
+        minute = _.padStart(minute, 2, '0');
+        second = _.padStart(second, 2, '0');
+
+        return day + '/' + month + '/' + year + ' ' + hour + ':' + minute + ':' + second;
     },
     deactivate: (object) => {
-        const returnFalse = () => { return false; }
-        return _.update(object, 'active', returnFalse);
+        return _.update(object, 'active', _.stubFalse());
     },
     difference: (toInspect, toExclude) => {
         return _.differenceWith(toInspect, toExclude, _.isEqual);
     },
     hash: (data) => {
-        let dataString = JSON.stringify(data);
+        const dataString = JSON.stringify(data);
         let md5sum = crypto.createHash('md5');
         md5sum.update(dataString);
         return md5sum.digest('hex');
@@ -46,7 +35,7 @@ module.exports = {
         return _.replace(id.toString(), /\//, '').toString();
     },
     splitAddress: (address) => {
-        let splitAddress = address.split(/(,| NSW )/);
+        const splitAddress = address.split(/(,| NSW | ACT )/);
         splitAddress = splitAddress.map((addressPortion) => {
             return addressPortion.trim();
         });
