878ad5e195666e5b96d7a45c2d9a7724d19d95d3
Jason Huang <sixth.shifts@gmail.com>
2018-06-28 21:49:57 +1000

    Added database.js

---
+++ api/firebase/database.js
@@ -0,0 +1,25 @@
+const admin = require('firebase-admin');
+const utils = require('../../utils');
+
+module.exports = class Database {
+
+    constructor() {
+        this.database = null;
+        this.brandsData = null;
+        this.fueltypesData = null;
+        this.stationsData = null;
+    }
+
+    isInitialised() {
+        return !!this.database && !!this.brandsData && !!this.fueltypesData && !!this.stationsData;
+    }
+
+    async init(firebaseCredentials) {
+        admin.initializeApp({
+            credential: admin.credential.cert(firebaseCredentials),
+            databaseURL: "https://petrol-patrol.firebaseio.com"
+        });
+        this.database = admin.database();
+    }
+
+};
