4e1a4db405633f84a36355d59308c809cabdf9a5
Jason Huang <sixth.shifts@gmail.com>
2018-07-04 20:18:31 +1000

    Added documentation to util functions

--- util/time.js
+++ util/time.js
@@ -1,21 +1,61 @@
 const _ = require('lodash');
 const moment = require('moment');
 
+const formatString = 'DD/MM/YYYY hh:mm:ss A';
+
+/**
+ * Determines the time difference in days between two moment objects
+ * 
+ * @param {moment} start A moment object representing the start of the time range
+ * @param {moment} end A moment object representing the end of the time range
+ * @returns {number} The time difference in days
+ */
+const diff = (start, end) => {
+    return end.diff(start, 'days');
+};
+
+/**
+ * Fetches a moment object representing epoch time
+ * 
+ * @returns {moment} A moment object representing epoch time
+ */
+const epoch = () => {
+    return moment(0);
+};
+
+/**
+ * Fetches a moment object representing current time
+ * 
+ * @returns {moment} A moment object representing current time
+ */
+const now = () => {
+    return moment();
+};
+
+/**
+ * Parses a string-based timestamp into a moment object representing the same time
+ * 
+ * @param {string} timestamp String-based timestamp
+ * @returns {moment} A moment object representing the same time as the string-based timestamp
+ */
+const parseTimestamp = (timestamp) => {
+    return moment(timestamp, formatString);
+};
+
+/**
+ * Parses a integer-based unix time into a moment object representing the same time
+ * 
+ * @param {string} unixTimeInSeconds Unix time since epoch in seconds
+ * @returns {moment} A moment object representing the same time as the unix time
+ */
+const parseUnix = (unixTimeInSeconds) => {
+    return moment(unixTimeInSeconds * 1000);
+};
+
 module.exports = {
-    formatString: 'DD/MM/YYYY hh:mm:ss A',
-    epoch() {
-        return moment(0);
-    },
-    now() {
-        return moment();
-    },
-    parseTimestamp(timestamp) {
-        return moment(timestamp, module.exports.formatString);
-    },
-    parseUnix(unixTimeInSeconds) {
-        return moment(unixTimeInSeconds * 1000);
-    },
-    diff(start, end) {
-        return end.diff(start, 'days');
-    },
+    diff,
+    epoch,
+    now,
+    parseTimestamp,
+    parseUnix,
 }
--- utils.js
+++ utils.js
@@ -2,71 +2,129 @@ const _ = require('lodash');
 const constants = require('./constants');
 const crypto = require('crypto');
 
-module.exports = {
-    deactivate: (object) => {
-        return _.update(object, 'active', () => { return false });
-    },
-    difference: (toInspect, toExclude) => {
-        return _.differenceWith(toInspect, toExclude, _.isEqual);
-    },
-    emptyPromise: (val = null) => {
-        return new Promise((resolve) => { resolve(val); });
-    },
-    geoEncode: (latitude, longitude) => {
-        let hash = '';
-        let bits = 0;
-        let numBits = 0;
-        const latitudeRange = {
-            min: -90,
-            max: 90
-        };
-        const longitudeRange = {
-            min: -180,
-            max: 180
-        };
-        let alternate = false;
-        while (hash.length < constants.geohashPrecision) {
-            const latOrLng = alternate ? latitude : longitude;
-            const range = alternate ? latitudeRange : longitudeRange;
-            const midRange = (range.min + range.max) / 2;
-            if (latOrLng > midRange) {
-                bits = (bits << 1) + 1;
-                range.min = midRange;
-            } else {
-                bits = (bits << 1) + 0;
-                range.max = midRange;
-            }
-            alternate = !alternate;
-            if (numBits < 4) {
-                numBits++;
-            } else {
-                numBits = 0;
-                hash += constants.geohashCharset[bits];
-                bits = 0;
-            }
-        }
-        return hash;
-    },
-    hash: (data) => {
-        const dataString = JSON.stringify(data);
-        let md5sum = crypto.createHash('md5');
-        md5sum.update(dataString);
-        return md5sum.digest('hex');
-    },
-    sanitiseID: (id) => {
-        return _.replace(id.toString(), /\//, '').toString();
-    },
-    splitAddress: (address) => {
-        let splitAddress = address.split(/(,| NSW | ACT )/);
-        splitAddress = splitAddress.map((addressPortion) => {
-            return addressPortion.trim();
-        });
+/**
+ * Returns an object with the 'active' property set to false
+ * 
+ * @param {object} object An object that is to be disabled
+ * @returns {object} An object with the active property set to false
+ */
+const deactivate = (object) => {
+    return _.update(object, 'active', () => { return false });
+};
 
-        return {
-            "street": splitAddress[0],
-            "suburb": splitAddress[2],
-            "state": splitAddress[3],
-            "postcode": splitAddress[4]
+/**
+ * Determines the left outer join difference between the toInspect and toExclude arrays 
+ * 
+ * @param {[object]} toInspect The array to inspect elements against
+ * @param {[object]} toExclude The array to exclude elements against
+ * @returns {[object]} An array containing the remainder of the toInspect array after subtracting the toExclude array
+ */
+const difference = (toInspect, toExclude) => {
+    return _.differenceWith(toInspect, toExclude, _.isEqual);
+};
+
+/**
+ * Returns an empty promise object
+ * 
+ * @param {*} val A value to return upon resolving the promise
+ * @returns {*} A promise that will resolve to the inputted value
+ */
+const emptyPromise = (val = null) => {
+    return new Promise((resolve) => { resolve(val); });
+};
+
+/**
+ * Encodes a latitude and longitude tuple into a geohash
+ * 
+ * @param {number} latitude 
+ * @param {number} longitude 
+ * @returns {string} A geohash value encoding the latitude and longitude values
+ */
+const geoEncode = (latitude, longitude) => {
+    let hash = '';
+    let bits = 0;
+    let numBits = 0;
+    const latitudeRange = {
+        min: -90,
+        max: 90
+    };
+    const longitudeRange = {
+        min: -180,
+        max: 180
+    };
+    let alternate = false;
+    while (hash.length < constants.geohashPrecision) {
+        const latOrLng = alternate ? latitude : longitude;
+        const range = alternate ? latitudeRange : longitudeRange;
+        const midRange = (range.min + range.max) / 2;
+        if (latOrLng > midRange) {
+            bits = (bits << 1) + 1;
+            range.min = midRange;
+        } else {
+            bits = (bits << 1) + 0;
+            range.max = midRange;
+        }
+        alternate = !alternate;
+        if (numBits < 4) {
+            numBits++;
+        } else {
+            numBits = 0;
+            hash += constants.geohashCharset[bits];
+            bits = 0;
         }
     }
+    return hash;
+};
+
+/**
+ * Generates an md5 hash from the input
+ * 
+ * @param {object} data The data to be hashed
+ * @returns {string} The hash value of the input
+ */
+const hash = (data) => {
+    const dataString = JSON.stringify(data);
+    let md5sum = crypto.createHash('md5');
+    md5sum.update(dataString);
+    return md5sum.digest('hex');
+};
+
+/**
+ * Sanitises an id value
+ * 
+ * @param {string} id The value to be sanitised
+ * @returns {string} The sanitised input
+ */
+const sanitiseID = (id) => {
+    return _.replace(id.toString(), /\//, '').toString();
+};
+
+/**
+ * Splits a composite address string into its components
+ * 
+ * @param {string} address The address string to be split
+ * @returns {object} An object containing the components of the address string
+ */
+const splitAddress = (address) => {
+    let splitAddress = address.split(/(,| NSW | ACT )/);
+    splitAddress = splitAddress.map((addressPortion) => {
+        return addressPortion.trim();
+    });
+
+    return {
+        "street": splitAddress[0],
+        "suburb": splitAddress[2],
+        "state": splitAddress[3],
+        "postcode": splitAddress[4]
+    }
+};
+
+module.exports = {
+    deactivate,
+    difference,
+    emptyPromise,
+    geoEncode,
+    hash,
+    sanitiseID,
+    splitAddress,
 }
